6d6f9ffe72f4fcf0227c649b011a27d674284662
april-yyt <aprilytyang@gmail.com>
2023-04-30 10:44:56 -0700

    debug

--- app/views.py
+++ app/views.py
@@ -4,13 +4,150 @@ import redis
 import os
 import openai
 from dotenv import load_dotenv
-from tasks import analyze_spending_income
-from celery.result import AsyncResult
-from parse_df import get_monthly_expenses
+# import parse_df
+import requests
 
+# Imports for parse df
+import re
+import pandas as pd
+import numpy as np 
+# from transformers import pipeline
+from typing import Union, List, Callable
+import json
+import os
+import openai
+openai.api_key = os.environ.get('CHATGPT_API_KEY')
+from io import StringIO
+
+openai.api_key = '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'
 
 load_dotenv()
 db = redis.Redis(host='localhost', port=6379, db=0)
+
+def clean_text(text: str): 
+    
+    """
+    Removes the non-essential information about an expense text
+    """
+    
+    # Remove random words
+    text = text.replace('PURCHASE', '')
+    text = text.replace('AUTHORIZED', '')
+    text = text.replace('RECURRING', '')
+    text = text.replace("PAYMENT", '')
+    text = text.replace("CARD", '')
+    text = text.replace("ON", '')
+    
+    # Remove dates 
+    date_pattern = r"\d{2}/\d{2}"
+    text = re.sub(date_pattern, "", text)
+    
+    # Remove cities 
+    cities = ['SAN FRANCISCO', 'NEW YORK', 'LOS ANGELES']
+
+    # Define a regular expression pattern to match any of the city names in the list
+    city_pattern = '|'.join(cities)
+
+    # Replace all instances of the city pattern with an empty string
+    text = re.sub(city_pattern, "", text)
+    
+    # Remove long valeus 
+    pattern = r'\s[PS]\d+\s'
+    text = re.sub(pattern, '', text)
+
+    # Strip 
+    text = text.strip()
+
+    return text
+
+def load_and_clean_csv(path: str = None, df: str = None): 
+    
+    """
+    Loads a CSV and tries to remove information not about the vendor
+    Params: 
+        - path (str): the path to a csv file 
+    Returns: 
+        - df (pd.DataFrame): the cleaned pandas df
+        - prompt (str): the completed prompt template
+    """
+
+    # Parse the CSV file
+    csv_data = db.get('uploaded_csv').decode('utf-8')
+    df = pd.read_csv(StringIO(csv_data))[['Date', 'Amount', 'Expense']]
+
+    # csv_data = csv_data.decode('utf-8')
+    # csv_rows = csv_data.split('\n')
+    # for row in csv_rows:
+    #     csv_fields = row.split(',')
+    #     # Process each field as necessary
+    
+    # Load CSV if need to 
+    # if (path):
+    #     df = pd.read_csv(path)[['Date', 'Amount', 'Expense']]
+    
+    # Clean text
+    df['Expense'] = df['Expense'].apply(clean_text)
+    
+    # Make columns lowercase
+    df.columns = df.columns.str.lower()
+
+    # Add column for month
+    df['month'] = pd.to_datetime(df['date']).dt.month
+
+
+    
+    prompt = """
+    I'm going to give you a list of bank statement expenses with some text removed (to shorten the token size). 
+
+    Your task is to classify each of these into one of the following categories:
+
+    ['food', 'fitness', 'travel', 'education', 'entertainment']
+
+    If the expense does not fall into one of those categories, return 'other'.
+
+    Please return JSON format like this: {1: 'rent', 2: 'food', 3: 'other'}
+
+    PLEASE MAKE SURE YOU ASSIGN EXACTLY ONE LABEL TO EACH EXPENSE.
+
+    Here are the expenses:
+    """
+
+    for idx, row in df.iterrows(): 
+        prompt += f'\n {idx+1}: {row["expense"]}'
+
+    
+    return df, prompt
+
+def get_monthly_expenses(df): 
+    
+    """
+    Gets the monthly expenses for a given df
+    Params: 
+        - df (pd.DataFrame): the expenses df
+    Returns: 
+        - monthly_expenses (dict): dictionary of monthly expenses by category
+    """
+
+    # Load in the data and generate prompt
+    df, prompt = load_and_clean_csv(df = df)
+
+    # Run query on gpt-3.5-turb 
+    labels = openai.ChatCompletion.create(model = 'gpt-3.5-turbo',
+                                          messages = [
+                                            {"role": "user", "content": prompt},
+                                          ])
+    
+    # Convert the string of lists to list 
+    labels = eval(labels['choices'][0]['message']['content'])
+
+    # Add predictions to df
+    df['label'] = list(labels.values())
+
+    # Double groupby to get monthly expenses 
+    monthly_expenses = df.groupby(['category', 'month'])['amount'].sum().groupby('category').mean().to_dict()
+
+    return monthly_expenses
+
 
 @app.route('/')
 def index():
@@ -34,17 +171,22 @@ def submit():
     if request.method == 'POST':
 
         # Grab file upload and user_id
-        cc_history =  request.form['cc-upload']
+        cc_history =  request.files['cc_upload']
+        if cc_history:
+            # Save the file to Redis
+            db.set('uploaded_csv', cc_history.read())
+            
+        
         user_id = request.form['user_id']
 
         # Extract monthly expenses 
-        monthly_expenses = get_monthly_expenses(cc_history)
+        # monthly_expenses = get_monthly_expenses(cc_history)
         
-        # Insert to redis DB
-        expense_types = ['food', 'fitness', 'travel', 'education', 'entertainment']
-        for expense_type in expense_types: 
-            val = monthly_expenses.get(expense_type)
-            db.hset(user_id, expense_type, val)
+        # # Insert to redis DB
+        # expense_types = ['food', 'fitness', 'travel', 'education', 'entertainment']
+        # for expense_type in expense_types: 
+        #     val = monthly_expenses.get(expense_type)
+        #     db.hset(user_id, expense_type, val)
 
 
         # Save user data to Redis database
@@ -54,7 +196,7 @@ def submit():
         db.hset(user_id, 'annual_income', request.form['annual_income'])
         db.hset(user_id, 'rent', request.form['rent'])
         db.hset(user_id, 'location', request.form['location'])
-        db.hset(user_id, 'investment_goal', request.form['goal'])
+        db.hset(user_id, 'investment_goal', request.form['investment_goal'])
         db.hset(user_id, 'investment_proportion', request.form['allocation_proportion'])
         db.hset(user_id, 'goal_achievement_time', request.form['time'])
         db.hset(user_id, 'risk_tolerance', request.form['risk_tolerance'])
@@ -73,7 +215,7 @@ def submit():
         elif request.form['investment_goal'] == 'college':
             db.hset(user_id, 'total_savings', request.form['total_savings'])
 
-    return redirect(url_for('success'))
+    return redirect(url_for('success', user_id=user_id))
 
 
 def generate_prompt(user_id):
@@ -93,12 +235,20 @@ def generate_prompt(user_id):
     investment_goal = (db.hget(user_id, 'investment_goal').decode())
     goal_achieve_time = (db.hget(user_id, 'goal_achievement_time').decode())
 
-    Rent = (db.hget(user_id, 'rent').decode())
-    Food = (db.hget(user_id, 'food').decode())
-    Fitness = (db.hget(user_id, 'fitness').decode())
-    Travel = (db.hget(user_id, 'travel').decode())
-    Education = (db.hget(user_id, 'education').decode())
-    Entertainment = (db.hget(user_id, 'entertainment').decode())
+    # Rent = (db.hget(user_id, 'rent').decode())
+    # Food = (db.hget(user_id, 'food').decode())
+    # Fitness = (db.hget(user_id, 'fitness').decode())
+    # Travel = (db.hget(user_id, 'travel').decode())
+    # Education = (db.hget(user_id, 'education').decode())
+    # Entertainment = (db.hget(user_id, 'entertainment').decode())
+
+    Rent = 3003
+    Food = 1000
+    Fitness = 567
+    Travel = 80
+    Education = 300
+    Entertainment = 400
+
 
 
     investment_proportion = int(db.hget(user_id, 'investment_proportion').decode())
@@ -160,7 +310,7 @@ def get_result_from_GPT(user_id):
         'stop': '\n'
     }
 
-    response = request.post(CHATGPT_API_ENDPOINT, headers=headers, json=data)
+    response = requests.post(CHATGPT_API_ENDPOINT, headers=headers, json=data)
     response.raise_for_status()
     result = response.json()
 
@@ -191,9 +341,15 @@ def get_result_from_GPT(user_id):
         return 'Unable to complete prompt', 500
 
 
-@app.route('/success')
-def success():
-    return render_template('success.html')
+@app.route('/success/<user_id>')
+def success(user_id):
+    # user_id = db.get('user_id')
+    # if user_id:
+    #     user_id = user_id.decode('utf-8')
+
+    result = chat_gpt_result(user_id)
+
+    return render_template('success.html', user_id=user_id, result = result)
 
 
 @app.route('/get_response', methods=['GET'])
